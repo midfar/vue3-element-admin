9d65feaec3f888ee8b1cf27cfde6cf72e33d77ec
midfar <midfar@qq.com>
2023-03-13 09:58:23 +0800

    update: dependency

--- src/views/clipboard/index.vue
+++ src/views/clipboard/index.vue
@@ -19,7 +19,7 @@
 
 <script>
 import clip from '@/utils/clipboard'; // use clipboard directly
-import clipboard from '@/directive/clipboard/index.js'; // use clipboard by v-directive
+import clipboard from '@/directive/clipboard'; // use clipboard by v-directive
 import { defineComponent, markRaw } from 'vue';
 import { ElMessage } from 'element-plus';
 import { Document } from '@element-plus/icons-vue';
