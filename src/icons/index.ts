2629456aaa928d7c83a84ee80b2083f0346a3dc4
midfar <midfar@qq.com>
2025-08-01 18:07:11 +0800

    fix: unused code

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,5 +1,5 @@
-import SvgIcon from '@/components/SvgIcon/index.vue';// svg component
+import SvgIcon from '@/components/SvgIcon/index.vue'; // svg component
 
-import.meta.glob('./svg/*.svg', { eager: true });
+// import.meta.glob('./svg/*.svg', { eager: true });
 
 export default SvgIcon;
